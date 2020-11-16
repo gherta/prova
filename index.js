833e8bc737c12202ad6673d60a2b1340516c37bd
gherta <32447809+gherta@users.noreply.github.com>
2020-11-16 19:44:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const handlebars = require('express-handlebars');
+app.set('view engine', 'hbs');
+app.engine('hbs', handlebars({
+    layoutsDir: __dirname + '/views/layouts',
+    extname: 'hbs',
+    defaultLayout: 'planB',
+    partialsDir: __dirname + '/views/partials/'
+}));
+app.use(express.static('public'))
+app.get('/', (req, res) => {
+    res.render('main', {layout: 'index'});});
+app.listen(port, () => console.log(`App listening to port ${port}`));
